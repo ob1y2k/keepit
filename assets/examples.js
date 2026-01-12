897ae132c0136c13c9809d4f31765c517f4b87d4
ob1y2k <ob1y2k@users.noreply.github.com>
2026-01-12 17:17:30 +0100

    ready

--- assets/examples.js
+++ assets/examples.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
+
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
+
+	$("#cookies").click(function(){
+
+		var cookie_value = Number(Cookies.get('keepit_cookie'));
+		//console.log("cookiescookiescookiescookiescookies " + cookie_value)
+		if(cookie_value > 0 ) {		
+			Cookies.set('keepit_cookie', cookie_value+1)
+			$("#cookieText").html(cookie_value+1+" cookies taken");
+		} else {
+			Cookies.set('keepit_cookie', 1)
+			$("#cookieText").html("1 cookie taken");
+		}		
+
+
+	});	
+
 });
